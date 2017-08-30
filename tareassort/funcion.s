8c2228d2888d7ba33ac4e6a391061176edb39a37
root <root@localhost.localdomain>
2017-08-30 14:32:47 +0000

    tarea2

---
+++ tareassort/funcion.s
@@ -0,0 +1,30 @@
+.section .data
+
+.section .text
+
+.globl power
+
+power:
+pushl %ebp
+movl %esp, %ebp
+subl $4, %esp
+
+movl 8(%ebp), %ebx
+movl 12(%ebp), %ecx
+movl %ebx, -4(%ebp)
+
+power_loop_start:
+cmpl $1, %ecx
+je end_power
+movl -4(%ebp), %eax
+imull %ebx, %eax
+movl %eax, -4(%ebp)
+decl %ecx
+jmp power_loop_start
+
+end_power:
+movl -4(%ebp), %eax
+movl %ebp, %esp
+popl %ebp
+ret
+
